eacff0ea0f0235fce6b5c895cd29357dfb7206c1
David Parra <davidc.parrar@gmail.com>
2022-06-17 18:43:41 -0500

    Add stage to redshift operator print version

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -32,43 +32,65 @@ dag = DAG(
 )
 
 start_operator = DummyOperator(
-    task_id="Begin_execution",
-    dag=dag,
+    task_id="Begin_execution", dag=dag, **default_args
 )
 
 stage_events_to_redshift = StageToRedshiftOperator(
-    task_id="Stage_events", dag=dag
+    task_id="Stage_events",
+    dag=dag,
+    s3_key="log-data",
+    s3_bucket="udacity-dend",
+    json_path="log_json_path.json",
+    region="us-west-1",
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id="redshift",
+    provide_context=True,
+    params={
+        "table": "stagingEvents",
+    },
+    **default_args
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id="Stage_songs", dag=dag
+    task_id="Stage_songs",
+    dag=dag,
+    s3_key="song-data",
+    s3_bucket="udacity-dend",
+    region="us-west-1",
+    aws_credentials_id="aws_credentials",
+    redshift_conn_id="redshift",
+    provide_context=True,
+    params={
+        "table": "stagingSongs",
+    },
+    **default_args
 )
 
-load_songplays_table = LoadFactOperator(
-    task_id="Load_songplays_fact_table", dag=dag
-)
+# load_songplays_table = LoadFactOperator(
+#     task_id="Load_songplays_fact_table", dag=dag
+# )
 
-load_user_dimension_table = LoadDimensionOperator(
-    task_id="Load_user_dim_table", dag=dag
-)
+# load_user_dimension_table = LoadDimensionOperator(
+#     task_id="Load_user_dim_table", dag=dag
+# )
 
-load_song_dimension_table = LoadDimensionOperator(
-    task_id="Load_song_dim_table", dag=dag
-)
+# load_song_dimension_table = LoadDimensionOperator(
+#     task_id="Load_song_dim_table", dag=dag
+# )
 
-load_artist_dimension_table = LoadDimensionOperator(
-    task_id="Load_artist_dim_table", dag=dag
-)
+# load_artist_dimension_table = LoadDimensionOperator(
+#     task_id="Load_artist_dim_table", dag=dag
+# )
 
-load_time_dimension_table = LoadDimensionOperator(
-    task_id="Load_time_dim_table", dag=dag
-)
+# load_time_dimension_table = LoadDimensionOperator(
+#     task_id="Load_time_dim_table", dag=dag
+# )
 
-run_quality_checks = DataQualityOperator(
-    task_id="Run_data_quality_checks", dag=dag
-)
+# run_quality_checks = DataQualityOperator(
+#     task_id="Run_data_quality_checks", dag=dag
+# )
 
-end_operator = DummyOperator(task_id="Stop_execution", dag=dag)
+# end_operator = DummyOperator(task_id="Stop_execution", dag=dag)
 
 start_operator >> stage_events_to_redshift
 start_operator >> stage_songs_to_redshift
